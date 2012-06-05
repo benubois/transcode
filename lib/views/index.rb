de914d0b3d9399dceae0c8de0b7a906823ec0788
Ben Ubois <ben@benubois.com>
2012-06-05 10:08:08 -0700

    Added basic index page.

--- lib/views/index.rb
+++ lib/views/index.rb
@@ -2,12 +2,14 @@ module Transcode
   module Views
     class Index < Layout
       
-      def title
-        "New title"
-      end
-      
-      def content
-        "Welcome! Mustache lives."
+      def movies
+        [
+          {
+            "path"   => "/Users/bb/Movies/A Movie", 
+            "name"   => "A Movie", 
+            "titles" => [{"title"=>1, "duration"=>2997, "feature"=>false, "blocks"=>[38547, 185494, 267415, 287751, 97471, 112126, 127829, 37225, 4]}, {"title"=>2, "duration"=>3389, "feature"=>false, "blocks"=>[39716, 151100, 171550, 177794, 267927, 140363, 355744, 44184, 4]}, {"title"=>3, "duration"=>3307, "feature"=>false, "blocks"=>[38542, 153650, 161623, 234624, 259135, 198478, 188046, 42403, 4]}]
+          }
+        ]
       end
       
     end
